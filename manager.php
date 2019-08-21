9b06c7fa8e5373a334384864be9da6cdee241ca1
Arnav Chawla <arnavchawla23@gmail.com>
2019-08-20 19:15:36 -0700

    Added Settings Page Functionality

--- manager.php
+++ manager.php
@@ -1,3 +1,4 @@
+
 <?php /**
  * Plugin Name: Classroom Manager
  * Plugin URI: http://www.mywebsite.com/my-first-plugin
@@ -106,7 +107,64 @@ function html_form_code() {
 	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
 	echo '</form>';
 }
+
+function my_awesome_page_display() {
+    global $wpdb;
+    $results = $wpdb->get_results( "SELECT * FROM wp_student"); // Query to fetch data from database table and storing in $results
+
+    if(!empty($results))                        // Checking if $results have some values or not
+    {
+        echo "<table width='100%' border='0'>"; // Adding <table> and <tbody> tag outside foreach loop so that it wont create again and again
+        echo "<tbody>";
+        foreach($results as $row){            //putting the user_ip field value in variable to use it later in update query
+        echo "<tr>";                           // Adding rows of table inside foreach loop
+        echo "<th>ID</th>" . "<td>" . $row->id . "</td>";
+        echo "</tr>";
+        echo "<td colspan='2'><hr size='1'></td>";
+        echo "<tr>";
+        echo "<th>Name</th>" . "<td>" . $row->name . "</td>";   //fetching data from user_ip field
+        echo "</tr>";
+        echo "<td colspan='2'><hr size='1'></td>";
+        echo "<tr>";
+        echo "<th>Email</th>" . "<td>" . $row->email . "</td>";
+        echo "</tr>";
+        echo "<td colspan='2'><hr size='1'></td>";
+        echo "<tr>";
+        echo "<th>Class</th>" . "<td>" . $row->class . "</td>";
+        echo "</tr>";
+        echo "<td colspan='2'><hr size='1'></td>";
+        echo "<tr>";
+        echo "<th>Phone</th>" . "<td>" . $row->phone . "</td>";
+        echo "</tr>";
+        echo "<td colspan='2'><hr size='1'></td>";
+        echo "<tr>";
+        echo "<th>Day</th>" . "<td>" . $row->day . "</td>";
+        echo "</tr>";
+
+        }
+        echo "</tbody>";
+        echo "</table>";
+
+    }
+    else
+    {
+        echo 'empty';
+    }
+
+}
+function awesome_page_create() {
+    $page_title = 'ClassroomManager';
+    $menu_title = 'Classroom Manager';
+    $capability = 'edit_posts';
+    $menu_slug = 'classroom-manager';
+    $function = 'my_awesome_page_display';
+    $icon_url = '';
+    $position = 61;
+
+    add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+}
+
 add_shortcode( 'form', 'html_form_code');
 add_shortcode( 'foobar', 'data_func' );
 register_activation_hook( __FILE__, 'do_first' );
-//register_activation_hook( __FILE__, 'do_thing' );
+add_action('admin_menu', 'awesome_page_create');
